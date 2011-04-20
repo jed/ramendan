e0cd54b9ff18319a85188d972c87c43e3f9c38e1
Jed Schmidt <tr@nslator.jp>
2011-04-21 02:10:41 +0900

    coming soon

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-var http = require('http');
+http = require( "http" )
  
-var server = http.createServer(function (req, res) {
-  res.writeHead(200, { "Content-Type": "text/plain" })
-  res.end("Hello world\n");
-});
+server = http.createServer( function( req, res ) {
+  res.writeHead( 200, { "Content-Type": "text/plain" } )
+  res.end( "Coming soon..." )
+})
  
-server.listen(process.env.PORT || 8001);
+server.listen( process.env.PORT || 8001 )
