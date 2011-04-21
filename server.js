72541fd13fe0a30d4a26f0c856ffbf4abcb15ad0
Jed Schmidt <tr@nslator.jp>
2011-04-21 20:10:44 +0900

    parsing tweets

--- server.js
+++ server.js
@@ -2,13 +2,25 @@ var http = require( "http" )
   , fs = require( "fs" )
   , TwitterNode = require( "twitter-node" ).TwitterNode
   , index = fs.readFileSync( "./index.html", "utf-8" )
-  , html = index.replace( "{{currently}}", "waiting for the next tweet to load." )
+  , lastTweet = fs.readFileSync( "./lastTweet.html", "utf-8" )
+  , html = index.replace( "{{lastTweet}}", lastTweet )
   , sites = {
       github:  "//github.com/jed",
       twitter: "//twitter.com/jedschmidt",
       flickr:  "//flickr.com/photos/tr4nslator"
     }
-
+    
+function htmlify( tweet ) {
+  return tweet.replace(
+    /([@]+[A-Za-z0-9-_]+)|([#]+[A-Za-z0-9-_]+)|(\b(?:https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig,
+    function( match, user, tag, url ) {
+      return 0,
+        user ? user.link( user.replace( "@", "http://twitter.com/" ) ) :
+        tag ? tag.link( tag.replace( "#", "http://search.twitter.com/search?q=%23" ) ) :
+        url ? url.link( url.replace( /^https?:\/\//, "" ) ) : ""
+    })
+}
+    
 new TwitterNode({
     user: "jedschmidt",
     password: "yaM@47hz(#z",
@@ -18,7 +30,10 @@ new TwitterNode({
     if ( tweet.in_reply_to_user_id ) return
     if ( tweet.user.id != 815114 ) return
     
-    html = index.replace( "{{currently}}", tweet.text )
+    lastTweet = htmlify( tweet.text )
+    html = index.replace( "{{lastTweet}}", lastTweet )
+
+    fs.writeFileSync( "./lastTweet.html", "utf-8" )    
   })
   .stream()
 
