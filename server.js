5fcce2a286ef5d1e3a4928131165d861dd389ca0
Jed Schmidt <tr@nslator.jp>
2011-04-21 13:59:59 +0900

    first rev

--- server.js
+++ server.js
@@ -1,25 +1,32 @@
 http = require( "http" )
 fs = require( "fs" )
 
-index = fs.readFileSync( "./index.html" )
+var sites = {
+  github:  "//github.com/jed",
+  twitter: "//twitter.com/jedschmidt",
+  flickr:  "//flickr.com/photos/tr4nslator"
+}
+
+var index
 
 server = http.createServer( function( req, res ) {
-  if ( req.url == "/" ) {
-    res.writeHead( 302, { "Location": "/coming.soon..." } )
-    res.end()
+  var url = req.url
+
+  index = fs.readFileSync( "./index.html" )
+
+  if ( ~url.indexOf( "/on/" ) ) {
+    url = sites[ url.substr( 4 ) ] || "//jed.is/"
+
+    res.writeHead( 302, { "Location": url } )
+    return res.end()
   }
   
-  else {
-    res.writeHead( 200, { "Content-Type": "text/html" } )
-    res.end( index )
-  }
+  res.writeHead( 200, { "Content-Type": "text/html" } )
+  res.end( index )
 })
  
 server.listen( process.env.PORT || 8001 )
 
 // https://gist.github.com/api/v1/json/gists/jed
 // http://github.com/api/v2/json/user/show/jed
-// 
-// jed.is/on/github
-// jed.is/on/twitter
-// jed.is/on/flickr
+// http://gravatar.com/avatar/7b72d5a18ab92129692e97a76a153fe0?s=48
