1a3fd3d9c4cf7d73ca020f23f4ee30c1fb010801
Jed Schmidt <tr@nslator.jp>
2011-04-21 17:51:57 +0900

    add columns and latest tweet

--- server.js
+++ server.js
@@ -1,18 +1,28 @@
-http = require( "http" )
-fs = require( "fs" )
+var http = require( "http" )
+  , fs = require( "fs" )
+  , TwitterNode = require( "twitter-node" ).TwitterNode
+  , index = fs.readFileSync( "./index.html", "utf-8" )
+  , html = index.replace( "{{currently}}", "waiting for the next tweet to load." )
+  , sites = {
+      github:  "//github.com/jed",
+      twitter: "//twitter.com/jedschmidt",
+      flickr:  "//flickr.com/photos/tr4nslator"
+    }
 
-var sites = {
-  github:  "//github.com/jed",
-  twitter: "//twitter.com/jedschmidt",
-  flickr:  "//flickr.com/photos/tr4nslator"
-}
-
-var index
+new TwitterNode({
+    user: "jedschmidt",
+    password: "yaM@47hz(#z",
+    follow: [ 815114 ]
+  })
+  .addListener( "tweet", function( tweet ) {
+    if ( tweet.in_reply_to_user_id ) return
+    
+    html = index.replace( "{{currently}}", tweet.text )
+  })
+  .stream()
 
 server = http.createServer( function( req, res ) {
   var url = req.url
-
-  index = fs.readFileSync( "./index.html" )
 
   if ( ~url.indexOf( "/on/" ) ) {
     url = sites[ url.substr( 4 ) ] || "//jed.is/"
@@ -22,7 +32,7 @@ server = http.createServer( function( req, res ) {
   }
   
   res.writeHead( 200, { "Content-Type": "text/html" } )
-  res.end( index )
+  res.end( html )
 })
  
 server.listen( process.env.PORT || 8001 )
