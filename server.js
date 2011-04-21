e324b29e3b510c12e4bbb98e9f0eeb8bf0c15d0a
Jed Schmidt <tr@nslator.jp>
2011-04-21 12:16:42 +0900

    landing page

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 http = require( "http" )
- 
+fs = require( "fs" )
+
+index = fs.readFileSync( "./index.html" )
+
 server = http.createServer( function( req, res ) {
-  res.writeHead( 200, { "Content-Type": "text/plain" } )
-  res.end( "Coming soon..." )
+  if ( req.url == "/" ) {
+    res.writeHead( 302, { "Location": "/coming.soon..." } )
+    res.end()
+  }
+  
+  else {
+    res.writeHead( 200, { "Content-Type": "text/html" } )
+    res.end( index )
+  }
 })
  
 server.listen( process.env.PORT || 8001 )
+
+// https://gist.github.com/api/v1/json/gists/jed
+// http://github.com/api/v2/json/user/show/jed
+// 
+// jed.is/on/github
+// jed.is/on/twitter
+// jed.is/on/flickr
